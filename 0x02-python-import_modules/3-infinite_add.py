b1d4f56354a12c45aafd751b55de93916b961b10
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2022-07-14 11:34:42 +0100

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__"
+    import sys
+    sum = 0
+    length = len(sys.argv)
+    if (length <= 1):
+        print("{}".format(sum))
+    else:
+        for number in range(1, length):
+            sum += int(sys.argv[number])
+        print("{}".format(sum))
