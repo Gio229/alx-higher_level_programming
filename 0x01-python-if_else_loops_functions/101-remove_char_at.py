6249bb806586a74396adef9393567ec0f42fd67c
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2022-07-14 08:48:51 +0100

    task 15

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#/usr/bin/python3
+def remove_char_at(str, n):
+    str_r = ""
+    for letter in str:
+        if n >= len(str) or n < 0:
+            str_r = str
+            break
+        elif letter == str[n]:
+            continue
+        else:
+            str_r += letter
+    return str_r
