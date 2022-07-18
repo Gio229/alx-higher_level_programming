fbbdd0acf1417373fd99008fff986095aeaad855
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2022-07-18 19:36:44 +0100

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
      squareMatrix = []
-    for line in matrix:
+    for line in squareMatrix:
         squareMatrix.append([i**2 for i in line])
     return squareMatrix
