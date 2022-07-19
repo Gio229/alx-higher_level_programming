bcf9cf928429b0adc4da83a38de7cfce24a8eec8
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2022-07-19 18:27:45 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class
+"""
+
+
+class Square:
+    """
+    This class is a class to define suare object
+    """
+    def __init__(self, size):
+        """initialization function (constructor)
+            size (integer): square size
+        """
+        self.__size = size
