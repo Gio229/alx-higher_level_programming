4ce244d3947df53a0d0c0450c240659528ebdc7b
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2022-07-20 11:26:53 +0100

    task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class
+"""
+
+
+class Square:
+    """
+    This class is a class to define suare object
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """initialization function (constructor)
+            size (integer): square size
+        """
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """
+        Return the current square area
+        """
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """
+        getter for size attribute
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        setter for size attribute
+        """
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def my_print(self):
+        """
+        prints in stdout the square with the character #
+        """
+        if (self.__size == 0):
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
