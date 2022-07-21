8626b5e3ffa2f53c196aa1ba44837e5ea9861f71
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-07-21 09:22:24 +0100

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -25,14 +25,15 @@ class Square:
                 if (position[0] >= 0 and position[1] >= 0):
                     self.__position = position
                 else:
-                    raise TypeError("position must be a"
+                    raise TypeError("position must be a "
                                     "tuple of 2 positive integers")
             else:
-                raise TypeError("position must be a"
+                raise TypeError("position must be a "
                                 "tuple of 2 positive integers")
         else:
-            raise TypeError("position must be a"
+            raise TypeError("position must be a "
                             "tuple of 2 positive integers")
+
 
     def area(self):
         """
@@ -75,13 +76,13 @@ class Square:
                 if (value[0] >= 0 and value[1] >= 0):
                     self.__position = value
                 else:
-                    raise TypeError("position must be a tuple"
+                    raise TypeError("position must be a tuple "
                                     "of 2 positive integers")
             else:
-                raise TypeError("position must be a tuple"
+                raise TypeError("position must be a tuple "
                                 "of 2 positive integers")
         else:
-            raise TypeError("position must be a tuple"
+            raise TypeError("position must be a tuple  "
                             "of 2 positive integers")
 
     def my_print(self):
