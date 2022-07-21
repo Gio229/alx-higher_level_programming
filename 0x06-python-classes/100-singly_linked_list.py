14be1d649e5fe14931171c992531b8a1e06f9f01
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-07-21 15:34:41 +0100

    task 7

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -10,13 +10,13 @@ class Node:
     """
     def __init__(self, data, next_node=None):
         """initialization function (constructor)
-            data (integer): 
+            data (integer)
             next_node (Node): square position
         """
         if (type(data) != int):
             raise TypeError("data must be an integer")
         self.__data = data
-        
+
         if (type(next_node) == Node or next_node is None):
             self.__next_node = next_node
         else:
@@ -44,9 +44,9 @@ class Node:
         getter for next_node attribute
         """
         return self.__next_node
-    
+
     @next_node.setter
-    def data(self, value):
+    def next_node(self, value):
         """
         setter for next_node attribute
         """
@@ -70,16 +70,27 @@ class SinglyLinkedList:
         inserts a new Node into the correct sorted position in
         the list
         """
-        if self.__head is None:
+        if (self.__head is None):
             self.__head = Node(value)
         else:
             current = self.__head
             previous = current
-            while current and value > current.data:
+            while current.next_node is not None:
+                if ( value <= current.data):
+                    break
                 previous = current
                 current = current.next_node
-            if current is None:
-                previous.next_node = Node(value)
+            if current is self.__head:
+                if (value <= current.data):
+                    self.__head = Node(value, current)
+                elif current.next_node is None:
+                    current.next_node = Node(value)
+            elif current.next_node is None:
+                if (current.data < value):
+                    current.next_node = Node(value)
+                else:
+                    newNode = Node(value, current)
+                    previous.next_node = newNode
             else:
                 newNode = Node(value, current)
                 previous.next_node = newNode
@@ -93,7 +104,7 @@ class SinglyLinkedList:
         while current is not None:
             string += str(current.data)
             current = current.next_node
-            if current is None:
+            if current is not None:
                 string += '\n'
 
         return string
