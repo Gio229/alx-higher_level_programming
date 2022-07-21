4ff6d73a75ee917d11a59385d8d98dc6fc6ff20a
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-07-21 09:23:53 +0100

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,7 +34,6 @@ class Square:
             raise TypeError("position must be a "
                             "tuple of 2 positive integers")
 
-
     def area(self):
         """
         Return the current square area
