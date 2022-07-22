727d39d7a277fdd018d1eceef3a3306fba92f60e
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2022-07-22 11:17:26 +0100

    task 10

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module defines class MagicClass
+"""
+
+
+class MagicClass:
+    """
+    This class does exactly the same as the given Python bytecode
+    """
+    def __init__(self, radius=0):
+        """
+        constructor
+        """
+        if type(radius) is not int:
+            if type(radius) is not float:
+                raise TypeError("radius must be a number")
+            else:
+                self.__radius = radius
+        else:
+            sel.__radius = radius
+
+        return None
+
+    def area(self):
+        """
+        Return the area
+        """
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """
+        Return the circumference
+        """
+        return 2 * math.pi * self.__radius
